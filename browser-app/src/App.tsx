27cadd7534352547791403b20c02e6a11b9cfb28
Sigurd Ofstad <sigurd.ofstad@spond.com>
2023-12-27 22:15:06 +0100

    Getting webgl to work

--- browser-app/src/App.tsx
+++ browser-app/src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import { myDemo } from "./my-demo";
+
+async function initiateCanvas() {
+  await myDemo();
+}
+
 function App() {
+  useEffect(() => {
+    initiateCanvas();
+  }, []);
   return (
     <>
       <div>Hello world</div>
+      <canvas
+        style={{ border: "1px solid black" }}
+        id="canvas"
+        width="800"
+        height="600"
+      ></canvas>
     </>
   );
 }
---
+++ browser-app/src/my-demo/webgl-demo.ts
@@ -0,0 +1,59 @@
+export function initShaderProgram(
+    gl: WebGL2RenderingContext,
+    vsSource: string,
+    fsSource: string,
+): WebGLProgram {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // Create the shader program
+
+    const shaderProgram = gl.createProgram();
+    if (!shaderProgram) throw Error("Unable to create shader program");
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // If creating the shader program failed, alert
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert(
+            `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+                shaderProgram,
+            )}`,
+        );
+        throw Error("Unable to initialize the shader program");
+    }
+
+    return shaderProgram;
+}
+
+export function loadShader(
+    gl: WebGL2RenderingContext,
+    type: number,
+    source: string,
+): WebGLShader {
+    const shader = gl.createShader(type);
+    if (!shader) throw Error("Unable to create shader");
+
+    // Send the source to the shader object
+
+    gl.shaderSource(shader, source);
+
+    // Compile the shader program
+
+    gl.compileShader(shader);
+
+    // See if it compiled successfully
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(
+            `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
+        );
+        gl.deleteShader(shader);
+        throw Error("Unable to compile shader");
+    }
+
+    return shader;
+}
+
