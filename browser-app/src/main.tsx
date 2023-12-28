ab57fb988f45944212adcd7a8135356c4ba74902
Sigurd Ofstad <sigurd.ofstad@spond.com>
2023-12-28 21:24:36 +0100

    Integrating a routing system. Refactoring a bit and making 2x3x2 the new testing grounds and ground truth.

--- browser-app/src/main.tsx
+++ browser-app/src/main.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/route.tsx";
+import { ErrorPage } from "./ErrorPage.tsx";
+import Contact from "./routes/Contact.tsx";
+import { Demo2x2RedBlue } from "./demos/2x2/Demo2x2RedBlue.tsx";
+
+import "./index.css";
+import { Demo2x3x2RedAndBlue } from "./demos/2x3x2/Demo2x3x2RedAndBlue.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/contacts/:contactId",
+                element: <Contact />,
+            },
+            {
+                path: "/ml/demos/2x2",
+                element: <Demo2x2RedBlue />,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: "/ml/demos/new",
+                element: <Demo2x3x2RedAndBlue />,
+                errorElement: <ErrorPage />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
