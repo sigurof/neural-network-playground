31de2b0492823ca5caeee1d66ca6cb2b4652796b
Sigurd Ofstad <sigurd.ofstad@spond.com>
2023-12-31 00:01:48 +0100

    Removing erroneous backpropagation code

--- networks/src/main/kotlin/no/sigurof/ml/NeuralNetwork.kt
+++ networks/src/main/kotlin/no/sigurof/ml/NeuralNetwork.kt
@@ -77,112 +77,15 @@ class WeightsAndBiases(
 class NeuralNetwork(val weightsAndBiases: WeightsAndBiases) {
 
 
-    fun calculateGradientBackpropagation(inputVsOutputs: InputVsOutput): DoubleArray {
-        val gradient = DoubleArray(weightsAndBiases.data.size) { _ -> 0.0 }
-        val activations: List<DoubleArray> = evaluateActivations(inputVsOutputs.input)
-
-        // For every activation in the last activation layer
-        // sum up 2(a_i^L - e_i) ∇a_i^L for every i in the last layer
-        for (activationIndex in 0 until weightsAndBiases.weightsLayers.last().outputs) {
-            val activation: Double = activations.last()[activationIndex]
-            val expectedValue: Double = inputVsOutputs.output[activationIndex]
-            val twoAMinusE: Double = 2.0 * (activation - expectedValue);
-            val weightIndex: Int = weightsAndBiases.weightsLayers.lastIndex
-            val gradientOfActivation: DoubleArray =
-                calcGradientOfActivation(weightIndex, activationIndex, activation, activations.dropLast(1))
-            for (j in gradientOfActivation.indices) {
-                gradientOfActivation[j] *= twoAMinusE
-                gradient[j] += gradientOfActivation[j]
-            }
-        }
-        return gradient;
-    }
-
-    /**
-     * Evaluates the formula for the gradient of each activation, which is:
-     * ----------------------------------------------------------------------------
-     * ∇a_i^L = a_i^L (1 - a_i^L) ( b_i^L ∑_j[ w_ij^L a_j^(L-1)]) + ∑_j[∇a_j^(L+1)]
-     * ----------------------------------------------------------------------------
-     * where ∑_j[ things to sum go here ] denotes a sum over the nodes of layer L-1
-     * and where w and b are vectors in the many-dimensional weights and biases space.
-     * */
-    private fun calcGradientOfActivation(
-        weightsIndex: Int,
-        activationIndex: Int,
-        activation: Double,
-        activations: List<DoubleArray>,
-    ): DoubleArray {
-        // TODO Are you sure this is correct?
-        val sigmoidPrime = activation * (1 - activation) // this is the derivative of the sigmoid function
-        val directDependenciesGradient = DoubleArray(weightsAndBiases.data.size) { _ -> 0.0 }
-
-        val weightsLayer = weightsAndBiases.weightsLayers[weightsIndex]
-        val ownMatrixStartIndex = weightsLayer.startIndex
-        val cols = weightsLayer.matrix.cols
-        val ownMatrixRowStartIndex = ownMatrixStartIndex + activationIndex * cols
-        val biasIndex = ownMatrixRowStartIndex + cols - 1
-
-        // 1 - Set the weights gradient contributions
-        var previousLayerActivationIndex = 0
-        for (col in ownMatrixRowStartIndex..<biasIndex) {
-            directDependenciesGradient[col] = activations.last()[previousLayerActivationIndex] * sigmoidPrime
-            previousLayerActivationIndex++;
-        }
-
-        // 2 - Set the bias's gradient contribution:
-        directDependenciesGradient[biasIndex] = sigmoidPrime
-
-        // 3 Find the gradients of each activation in the previous layer, i.e. ∑_j[∇a_j^(L+1)]
-        // Also, stop recursing if we are at the first layer
-        val gradientContributionsOfPreviousLayers = DoubleArray(weightsAndBiases.data.size) { _ -> 0.0 }
-
-        // TODO I'm pretty sure this cuts off correctly, since it works for the 2x2 case. However, something might be off with the arguments.
-        if (weightsIndex > 0) {
-
-            for (col in ownMatrixRowStartIndex..<biasIndex) {
-                val nextActivationIndex = col - ownMatrixRowStartIndex
-                // TODO Clean up this usage of the weight.
-                val weight = weightsAndBiases.data[col]
-                gradientContributionsOfPreviousLayers.mutablyAddElementwise(
-                    calcGradientOfActivation(
-                        weightsIndex = weightsIndex - 1,
-                        activationIndex = nextActivationIndex,
-                        activation = activations.last()[nextActivationIndex],
-                        activations = activations.dropLast(1)
-                    )
-                )
-                for (i in gradientContributionsOfPreviousLayers.indices) {
-                    gradientContributionsOfPreviousLayers[i] *= (sigmoidPrime * weight)
-                }
-            }
-//            for (nextActivationIndex in 0 until weightsAndBiases.weightsLayers[weightsIndex - 1].outputs) {
-//                gradientContributionsOfPreviousLayers.mutablyAddElementwise(
-//                    // TODO Move the multiplication of w_ji^L to the outside of this call
-//                     calcGradientOfActivation(
-//                        weightsIndex = weightsIndex - 1,
-//                        activationIndex = nextActivationIndex,
-//                        activation = activations.last()[nextActivationIndex],
-//                        activations = activations.dropLast(1)
-//                    )
-//                )
-//                for (i in gradientContributionsOfPreviousLayers.indices) {
-//                    gradientContributionsOfPreviousLayers[i] *= sigmoidPrime;
-//                }
-//            }
-        }
-
-        return directDependenciesGradient.mutablyAddElementwise(gradientContributionsOfPreviousLayers)
-    }
-
     internal fun calculateCostFunction(trainingData: List<InputVsOutput>): Double {
         return trainingData.map { trainingDataPoint: InputVsOutput ->
-            val outputVector: DoubleArray = evaluateNetwork(trainingDataPoint.input)
-            var error = 0.0
-            for (i in outputVector.indices) {
-                val diff = outputVector[i] - trainingDataPoint.output[i]
-                error += diff * diff
+            val outputActivations: DoubleArray = evaluateActivations(trainingDataPoint.input).last()
+            var sumErrorsSquared = 0.0
+            for (i in outputActivations.indices) {
+                val activationMinusExpectation = outputActivations[i] - trainingDataPoint.output[i]
+                sumErrorsSquared += activationMinusExpectation * activationMinusExpectation
             }
-            error
+            sumErrorsSquared
         }.average()
     }
 
@@ -193,15 +96,6 @@ class NeuralNetwork(val weightsAndBiases: WeightsAndBiases) {
             activations.add(elementwiseSigmoid(arrayProduct))
         }
         return activations
-    }
-
-    fun evaluateNetwork(input: DoubleArray): DoubleArray {
-        val activations: MutableList<DoubleArray> = mutableListOf(input)
-        for (layer in weightsAndBiases.weightsLayers) {
-            val arrayProduct: DoubleArray = layer.matrix * activations[layer.index].concat(1)
-            activations.add(elementwiseSigmoid(arrayProduct))
-        }
-        return activations.last()
     }
 
 
--- networks/src/test/kotlin/BackpropagationTest.kt
+++ networks/src/test/kotlin/BackpropagationTest.kt
@@ -13,241 +13,69 @@ import no.sigurof.ml.NeuralNetworkBuilder
 class BackpropagationTest : FreeSpec({
 
 
-    "testing an easy case" - {
-
-        val trainingData = listOf(
-            InputVsOutput(
-                input = doubleArrayOf(0.1, 0.5),
-                output = doubleArrayOf(0.0, 1.0)
-            )
-        )
-        val neuralNetworkBuilder = NeuralNetworkBuilder(
-            hiddenLayerDimensions = listOf(), trainingData = trainingData
-        )
-        val buildNetwork = neuralNetworkBuilder
-            .buildNetwork(
-                doubleArrayOf(
-                    0.0, 0.0, 0.0,
-                    0.0, 0.0, 0.0
-                )
-            )
-        val contentToString = buildNetwork.evaluateNetwork(trainingData.first().input).contentToString()
-        println("Network evaluated: " + contentToString + ", desired output: " + trainingData.first().output.contentToString())
-        val gradient = neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-
-
-
-        println("Auto Gradient: " + gradient.contentToString())
-
-        // Input activations
-        val a0f: Double = 0.1
-        val a1f: Double = 0.5
-
-        // Final activations
-        val a0L: Double = sigmoid(0.0);
-        val a1L: Double = sigmoid(0.0);
-
-        val e0: Double = 0.0;
-        val e1: Double = 1.0;
-
-
-        val X0: Double = 0.0;
-        val X1: Double = 0.0;
-        val sigmoid0: Double = sigmoid(X0);
-        val sigmoid1: Double = sigmoid(X1);
-        val sigmoidPrime0: Double = (1.0 - sigmoid0) * sigmoid0
-        val sigmoidPrime1: Double = (1.0 - sigmoid1) * sigmoid1
-
-        val grada0L: DoubleArray = sigmoidPrime0 * doubleArrayOf(a0f, a1f, 1.0, 0.0, 0.0, 0.0);
-        val grada1L: DoubleArray = sigmoidPrime1 * doubleArrayOf(0.0, 0.0, 0.0, a0f, a1f, 1.0);
-
-        val left = (a0L - e0) * grada0L
-        val right = (a1L - e1) * grada1L
-        val finalGradient = 2.0 * (left.addElementwise(right))
-        println("Manually calculated gradient: " + finalGradient.contentToString())
-        val buildNetwork1 = NeuralNetworkBuilder(
-            hiddenLayerDimensions = listOf(), trainingData = trainingData
-        )
-            .buildNetwork(
-                -finalGradient
-            )
-        val buildNetwork2 = buildNetwork1.evaluateNetwork(trainingData.first().input)
-        println(
-            buildNetwork2.contentToString()
-        )
-
-        // train it
-        println(
-            "Trained network: " + neuralNetworkBuilder.train(
-                false,
-                neuralNetworkBuilder::calculateGradientBackpropagation
-            )
-                .evaluateNetwork(trainingData.first().input).contentToString()
-        )
-    }
-
-
     "do the two methods of computing the gradient give the same result?" - {
 
-        "wrong backpropagation" - {
-            "easy case: for a 2 layers network" - {
-                val trainingData = listOf(
-                    InputVsOutput(
-                        input = doubleArrayOf(0.1, 0.5),
-                        output = doubleArrayOf(0.0, 1.0)
+
+        "easy case: for a 2 layers network" - {
+            val trainingData = listOf(
+                InputVsOutput(
+                    input = doubleArrayOf(0.1, 0.5),
+                    output = doubleArrayOf(0.0, 1.0)
+                )
+            )
+            val neuralNetworkBuilder = NeuralNetworkBuilder(
+                hiddenLayerDimensions = listOf(), trainingData = trainingData
+            )
+            val buildNetwork = neuralNetworkBuilder
+                .buildNetwork(
+                    doubleArrayOf(
+                        0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0
                     )
                 )
-                val neuralNetworkBuilder = NeuralNetworkBuilder(
-                    hiddenLayerDimensions = listOf(), trainingData = trainingData
-                )
-                val buildNetwork = neuralNetworkBuilder
-                    .buildNetwork(
-                        doubleArrayOf(
-                            0.0, 0.0, 0.0,
-                            0.0, 0.0, 0.0
-                        )
-                    )
-                val inefficientGradient =
-                    neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-                val backpropagationGradient =
-                    neuralNetworkBuilder.calculateGradientBackpropagation(buildNetwork, trainingData)
-                val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
-                val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
-                println("Inefficient     gradient: $inefficientGradientFormatted")
-                println("Backpropagation gradient: $backpropagationGradientFormatted")
-                inefficientGradient.zip(backpropagationGradient).forEach { (a, b) ->
-                    a / b shouldBe (1.0 plusOrMinus 1e-1)
-                }
-            }
-
-            "wrong backpropagation more complicated case: 3 layers" - {
-                val trainingData = listOf(
-                    InputVsOutput(
-                        input = doubleArrayOf(10.0, 5.0, 0.4),
-                        output = doubleArrayOf(0.0, 1.0, 0.0)
-                    )
-                )
-                val neuralNetworkBuilder = NeuralNetworkBuilder(
-                    hiddenLayerDimensions = listOf(3), trainingData = trainingData
-                )
-                val buildNetwork = neuralNetworkBuilder.populateWeightsAndBiasesRaw {
-                    Random.nextDouble(-1.0, 1.0)
-                }
-
-                val inefficientGradient =
-                    neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-                val backpropagationGradient =
-                    neuralNetworkBuilder.calculateGradientBackpropagation(buildNetwork, trainingData)
-                val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
-                val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
-                println("Inefficient     gradient: $inefficientGradientFormatted")
-                println("Backpropagation gradient: $backpropagationGradientFormatted")
-                val gradientsRatio = inefficientGradient.zip(backpropagationGradient).map { (a, b) ->
-                    a / b
-                }
-                println("Ratio between gradients (should be 1.0 +- an error): $gradientsRatio")
-
-                gradientsRatio.shouldMatchEach({
-                    it shouldBe (1.0 plusOrMinus 1e-1)
-                })
-            }
-
-            "wrong backpropagation more complicated case: 3 layers" - {
-                val trainingData = listOf(
-                    InputVsOutput(
-                        input = doubleArrayOf(10.0, 5.0),
-                        output = doubleArrayOf(0.0, 1.0)
-                    )
-                )
-                val neuralNetworkBuilder = NeuralNetworkBuilder(
-                    hiddenLayerDimensions = listOf(2), trainingData = trainingData
-                )
-                val buildNetwork = neuralNetworkBuilder.populateWeightsAndBiasesRaw {
-                    Random.nextDouble(-1.0, 1.0)
-                }
-
-                val inefficientGradient =
-                    neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-                val backpropagationGradient =
-                    neuralNetworkBuilder.calculateGradientBackpropagation(buildNetwork, trainingData)
-                val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
-                val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
-                println("Inefficient     gradient: $inefficientGradientFormatted")
-                println("Backpropagation gradient: $backpropagationGradientFormatted")
-                val gradientsRatio = inefficientGradient.zip(backpropagationGradient).map { (a, b) ->
-                    a / b
-                }
-                println("Ratio between gradients (should be 1.0 +- an error): $gradientsRatio")
-
-                gradientsRatio.shouldMatchEach({
-                    it shouldBe (1.0 plusOrMinus 1e-1)
-                })
+            val inefficientGradient =
+                neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
+            val backpropagationGradient =
+                neuralNetworkBuilder.calculateGradientBackpropagationNew(buildNetwork, trainingData)
+            val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
+            val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
+            println("Inefficient     gradient: $inefficientGradientFormatted")
+            println("Backpropagation gradient: $backpropagationGradientFormatted")
+            inefficientGradient.zip(backpropagationGradient).forEach { (a, b) ->
+                a / b shouldBe (1.0 plusOrMinus 1e-1)
             }
         }
 
-
-        "new backpropagation " - {
-
-            "easy case: for a 2 layers network" - {
-                val trainingData = listOf(
-                    InputVsOutput(
-                        input = doubleArrayOf(0.1, 0.5),
-                        output = doubleArrayOf(0.0, 1.0)
-                    )
+        "new backpropagation algorithm 3 layers" - {
+            val trainingData = listOf(
+                InputVsOutput(
+                    input = doubleArrayOf(10.0, 5.0, 5.0),
+                    output = doubleArrayOf(0.0, 1.0, 0.0)
                 )
-                val neuralNetworkBuilder = NeuralNetworkBuilder(
-                    hiddenLayerDimensions = listOf(), trainingData = trainingData
-                )
-                val buildNetwork = neuralNetworkBuilder
-                    .buildNetwork(
-                        doubleArrayOf(
-                            0.0, 0.0, 0.0,
-                            0.0, 0.0, 0.0
-                        )
-                    )
-                val inefficientGradient =
-                    neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-                val backpropagationGradient =
-                    neuralNetworkBuilder.calculateGradientBackpropagationNew(buildNetwork, trainingData)
-                val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
-                val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
-                println("Inefficient     gradient: $inefficientGradientFormatted")
-                println("Backpropagation gradient: $backpropagationGradientFormatted")
-                inefficientGradient.zip(backpropagationGradient).forEach { (a, b) ->
-                    a / b shouldBe (1.0 plusOrMinus 1e-1)
-                }
+            )
+            val neuralNetworkBuilder = NeuralNetworkBuilder(
+                hiddenLayerDimensions = listOf(3), trainingData = trainingData
+            )
+            val buildNetwork = neuralNetworkBuilder.populateWeightsAndBiasesRaw {
+                Random.nextDouble(-1.0, 1.0)
             }
 
-            "new backpropagation algorithm 3 layers" - {
-                val trainingData = listOf(
-                    InputVsOutput(
-                        input = doubleArrayOf(10.0, 5.0, 5.0),
-                        output = doubleArrayOf(0.0, 1.0, 0.0)
-                    )
-                )
-                val neuralNetworkBuilder = NeuralNetworkBuilder(
-                    hiddenLayerDimensions = listOf(3), trainingData = trainingData
-                )
-                val buildNetwork = neuralNetworkBuilder.populateWeightsAndBiasesRaw {
-                    Random.nextDouble(-1.0, 1.0)
-                }
-
-                val inefficientGradient = neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
-                val backpropagationGradient =
-                    neuralNetworkBuilder.calculateGradientBackpropagationNew(buildNetwork, trainingData)
-                val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
-                val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
-                println("Inefficient     gradient: $inefficientGradientFormatted")
-                println("Backpropagation gradient: $backpropagationGradientFormatted")
-                val gradientsRatio = inefficientGradient.zip(backpropagationGradient).map { (a, b) ->
-                    a / b
-                }
-                println("Ratio between gradients (should be 1.0 +- an error): $gradientsRatio")
-
-                gradientsRatio.shouldMatchEach({
-                    it shouldBe (1.0 plusOrMinus 1e-1)
-                })
+            val inefficientGradient =
+                neuralNetworkBuilder.calculateGradientInefficiently(buildNetwork, trainingData)
+            val backpropagationGradient =
+                neuralNetworkBuilder.calculateGradientBackpropagationNew(buildNetwork, trainingData)
+            val inefficientGradientFormatted = inefficientGradient.map { format("%.15f", it) }
+            val backpropagationGradientFormatted = backpropagationGradient.map { format("%.15f", it) }
+            println("Inefficient     gradient: $inefficientGradientFormatted")
+            println("Backpropagation gradient: $backpropagationGradientFormatted")
+            val gradientsRatio = inefficientGradient.zip(backpropagationGradient).map { (a, b) ->
+                a / b
             }
+            println("Ratio between gradients (should be 1.0 +- an error): $gradientsRatio")
+
+            gradientsRatio.shouldMatchEach({
+                it shouldBe (1.0 plusOrMinus 1e-1)
+            })
         }
 
     }
